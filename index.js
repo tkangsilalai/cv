cbc57f02384a3e1c6eba3b9cb657151c533eb0c3
Theerapat Kangsilalai <tkangsilalai@gmail.com>
2021-01-28 22:15:35 +0700

    html

---
+++ index.js
@@ -0,0 +1,21 @@
+$("#edu-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#edu").offset().top},
+        'fast');
+});
+$("#act-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#activities").offset().top},
+        'fast');
+});
+$("#skill-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#skills").offset().top},
+        'fast');
+});
+$("#proj-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#projects").offset().top},
+        'fast');
+});
+
